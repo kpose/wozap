d79e1ce8f02698e1567536b20443b6836065728a
kpose <ganihujude@gmail.com>
2019-03-27 05:45:26 +0100

    Connect Resolvers to GraphQL Server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
 import { ApolloServer } from 'apollo-server';
 import { typeDefs } from './data/schema';
 import { mocks } from './data/mocks';
+import { resolvers } from './data/resolvers';
 const PORT = 8080;
 // basic schema
+const server = new ApolloServer({
+  resolvers,
+  typeDefs
+  // mocks,
+});
 
 const server = new ApolloServer({ typeDefs, mocks });
 server
