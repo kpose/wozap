e3d0a4075c691333e8144c06965fbd1ca3b67cab
kpose <ganihujude@gmail.com>
2019-03-27 05:12:21 +0100

    design GraphQl schema

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
-import { ApolloServer, gql } from 'apollo-server';
+import { ApolloServer } from 'apollo-server';
+import { typeDefs } from './data/schema';
 const PORT = 8080;
 // basic schema
 const typeDefs = gql`
@@ -7,4 +8,6 @@ const typeDefs = gql`
   }
 `;
 const server = new ApolloServer({ typeDefs, mocks: true });
-server.listen({ port: PORT }).then(({ url }) => console.log(`🚀 Server ready at ${url}`));
+server
+  .listen({ port: PORT })
+  .then(({ url }) => console.log(`🚀 Server ready at ${url}`));
